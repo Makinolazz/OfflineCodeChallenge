0af661935f0095511adc52c8830e10bcc1eaa535
Franco Barranca <franco.barranca@hotmail.com>
2022-04-07 21:29:43 -0300

    initial commit
    Base project

---
+++ InputProcessor.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OfflineCodeChallenge
+{
+    public class InputProcessor
+    {
+        private int[] FirstArray { get; set; }
+        private int[] SecondArray { get; set; }
+        private int[] ResultArray { get; set; }
+        private bool isInputANegative;
+        private bool isInputBNegative;
+        private bool HasInvalidData;
+        public string Result { get; set; }
+
+
+        public string ProcessInput(string inputA, string inputB)
+        {
+            ValidateEmptyInputs(inputA, inputB);
+            VerifyNegativeInputs(ref inputA, ref inputB);
+
+            FirstArray = BuildIntArray(inputA);
+            SecondArray = BuildIntArray(inputB);
+
+            if (!HasInvalidData)
+            {
+                if (isInputANegative)
+                {
+                    SetNegativeValues(FirstArray);
+                }
+
+                if (isInputBNegative)
+                {
+                    SetNegativeValues(SecondArray);
+                }
+
+                Array.Reverse(SecondArray);
+                SumArraysItems();
+                PrintResult();
+                return ResultArray.ToString();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input data");
+                return "Invalid input data";
+            }            
+        }
+
+        private void SetNegativeValues(int[] storedArray)
+        {
+            for (int i = 0; i < storedArray.Length; i++)
+            {
+                storedArray[i] = storedArray[i] * -1;
+            }
+        }
+
+        private void ValidateArray(int[] array)
+        {
+            HasInvalidData = array.Any(x => x == -1) ? true : false;
+        }
+
+        private void VerifyNegativeInputs(ref string inputA, ref string inputB)
+        {
+            isInputANegative = IsNegative(inputA);
+            isInputBNegative = IsNegative(inputB);
+
+            inputA = RemoveMinusSign(inputA);
+            inputB = RemoveMinusSign(inputB);
+        }
+
+        private static bool IsNegative(string input)
+        {
+            return input[0].Equals('-') ? true : false;
+        }
+
+        private string RemoveMinusSign(string input)
+        {
+            input = input.Replace('-', ' ');
+            return input.Trim();
+        }
+
+        public string ProcessInput(int inputA, int inputB)
+        {
+            return ProcessInput(inputA.ToString(), inputB.ToString());
+        }
+
+        private void PrintResult()
+        {
+            for (int i = 0; i < ResultArray.Length; i++)
+            {
+                Console.Write(ResultArray[i]);
+            }
+        }
+
+        private void SumArraysItems()
+        {
+            int[] shortestArray;
+
+            if (FirstArray.Length <= SecondArray.Length)
+            {
+                ResultArray = SecondArray;
+                shortestArray = FirstArray;
+            }
+            else
+            {
+                ResultArray = FirstArray;
+                shortestArray = SecondArray;
+            }
+
+            for (int i = 0; i < shortestArray.Length; i++)
+            {
+                ResultArray[i] += shortestArray[i];
+            }
+        }
+
+        private int[] BuildIntArray(string input)
+        {
+            var resultArray = input.Trim().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            ValidateArray(resultArray);
+            return resultArray;
+        }
+
+        private void ValidateEmptyInputs(string inputA, string inputB)
+        {
+            if (string.IsNullOrEmpty(inputA) || string.IsNullOrEmpty(inputB))
+            {
+                Console.WriteLine("Inputs can't be empty");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OfflineCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables initialization
+            InputProcessor processor = new InputProcessor();
+            string stringInputA = "123";
+            string stringInputB = "123";
+
+            int intInputA = 123;
+            int intInputB = 123;
+
+            string result1 = processor.ProcessInput(stringInputA, stringInputB);
+            Console.WriteLine();
+            string result2 = processor.ProcessInput(intInputA, intInputB);
+            Console.WriteLine();
+
+            string stringNegativeInputA = "-123";
+            string stringNegativeInputB = "-123";
+
+            int intNegativeInputA = -123;
+            int intNegativeInputB = -123;
+
+            string result3 = processor.ProcessInput(stringNegativeInputA, stringNegativeInputB);
+            Console.WriteLine();
+            string result4 = processor.ProcessInput(intNegativeInputA, intNegativeInputB);
+            Console.WriteLine();
+
+
+            string stringMixedInputA = "-12abc3";
+            string stringMixedInputB = "-12abc3";
+
+            string result5 = processor.ProcessInput(stringMixedInputA, stringMixedInputB);
+            Console.WriteLine();
+        }
+    }
+}
